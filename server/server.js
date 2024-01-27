5630276d052b24ac4e81641c3300263beeac91d7
prawdziwyswiatmarzen <prawdziwyswiatmarzen@gmail.com>
2024-01-27 23:01:52 +0100

    server-website-form

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
@@ -9,6 +8,7 @@ import moviesRouter from "./Routes/MoviesRouter.js"
 import { Server } from 'socket.io';
 import http from 'http';
 import mqtt from 'mqtt';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -37,76 +37,111 @@ const io = new Server(server, {
     }
 });
 
-server.listen(PORT, ()  => {
-    console.log(`Server running in http://localhost/${PORT}`);
-});
-
 const mqttClient = mqtt.connect(process.env.MQTT_BROKER);
 
 mqttClient.on('connect', () => {
   console.log('Connected to MQTT broker');
 });
 
-const roomsMap = new Map();
-
 const defaultRooms = ['The Best Fandom', 'The Best Actor', 'The worst Actor'];
+const feedbackQuestions = [
+  { id: 1, text: 'How would you rate the overall design of our website?' },
+  { id: 2, text: 'Did you find the navigation on our website intuitive?' },
+  { id: 3, text: 'What features would you like to see added to our website?' },
+];
+
+const roomsMap = new Map();
 
 defaultRooms.forEach((roomName) => {
   roomsMap.set(roomName, { users: [] });
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.emit('rooms', getAvailableRooms());
-
-  socket.on('send-message', ({ topic, message }) => {
-    mqttClient.publish(topic, JSON.stringify(message));
-    io.to(topic).emit('send-message', { topic, message });
-  });
-
-  socket.on('add-room', ({ roomName }) => {
-    if (!roomsMap.has(roomName)) {
-      roomsMap.set(roomName, { users: [] });
-      io.emit('room-added', { roomName });
-      io.emit('rooms', getAvailableRooms());
-    }
-  });
-
-  socket.on('join-room', ({ roomName, username }) => {
-    const room = roomsMap.get(roomName);
-    if (room && room.users.includes(username)) {
-      console.log('User already in the room');
-    } else {
-      socket.join(roomName);
+export const SocketConnection = (socket) => {
+    console.log('A user connected');
+  
+    socket.emit('rooms', getAvailableRooms());
+    socket.emit('feedback-questions', feedbackQuestions);
+  
+    socket.on('submit-feedback', ({ answers, userEmail }) => {
+        console.log('Received feedback:', answers, 'from user:', userEmail);
+    
+        const feedbackData = fs.readFileSync('feedback.log', 'utf8');
+        const existingFeedback = feedbackData.split('\n').some((line) => {
+        try {
+            const feedback = JSON.parse(line);
+            return feedback.userEmail === userEmail;
+        } catch (error) {
+            return false;
+        }
+        });
+    
+        if (existingFeedback) {
+        console.log('User already submitted feedback.');
+        socket.emit('feedback-submitted', { success: false, message: 'User already submitted feedback.' });
+        return;
+        }
+    
+        fs.appendFileSync('feedback.log', JSON.stringify({ ...answers, userEmail }) + '\n');
+    
+        socket.emit('feedback-submitted', { success: true });
+    });
+  
+  
+  
+    socket.on('send-message', ({ topic, message }) => {
+      mqttClient.publish(topic, JSON.stringify(message));
+      io.to(topic).emit('send-message', { topic, message });
+    });
+  
+    socket.on('add-room', ({ roomName }) => {
       if (!roomsMap.has(roomName)) {
         roomsMap.set(roomName, { users: [] });
+        io.emit('room-added', { roomName });
+        io.emit('rooms', getAvailableRooms());
       }
-      roomsMap.get(roomName).users.push(username);
-      io.to(roomName).emit('user-joined', { roomName, username });
-      io.emit('rooms', getAvailableRooms());
-    }
-  });
-
-  socket.on('leave-room', ({ roomName, username }) => {
-    socket.leave(roomName);
-    const room = roomsMap.get(roomName);
-    if (room) {
-      room.users = room.users.filter((user) => user !== username);
-      io.to(roomName).emit('user-left', { roomName, username });
-      io.emit('rooms', getAvailableRooms());
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+    });
+  
+    socket.on('join-room', ({ roomName, username }) => {
+      const room = roomsMap.get(roomName);
+      if (room && room.users.includes(username)) {
+        console.log('User already in the room');
+      } else {
+        socket.join(roomName);
+        if (!roomsMap.has(roomName)) {
+          roomsMap.set(roomName, { users: [] });
+        }
+        roomsMap.get(roomName).users.push(username);
+        io.to(roomName).emit('user-joined', { roomName, username });
+        console.log(roomsMap)
+        io.emit('rooms', getAvailableRooms());
+      }
+    });
+  
+    socket.on('leave-room', ({ roomName, username }) => {
+      socket.leave(roomName);
+      const room = roomsMap.get(roomName);
+      if (room) {
+        room.users = room.users.filter((user) => user !== username);
+        io.to(roomName).emit('user-left', { roomName, username });
+        io.emit('rooms', getAvailableRooms());
+      }
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+}
 
 const getAvailableRooms = () => {
-  const rooms = Array.from(roomsMap.keys()).map((roomName) => ({
-    name: roomName,
-    users: roomsMap.get(roomName).users,
-  }));
-  return rooms;
+    const rooms = Array.from(roomsMap.keys()).map((roomName) => ({
+      name: roomName,
+      users: roomsMap.get(roomName).users,
+    }));
+    return rooms;
 };
+
+io.on('connection', SocketConnection);
+server.listen(PORT, ()  => {
+  console.log(`Server running in http://localhost/${PORT}`);
+});
+
